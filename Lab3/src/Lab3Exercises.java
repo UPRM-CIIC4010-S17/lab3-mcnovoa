77391326d6be6ec92025d534cb49e7489e1209e5
4015c11 <4015c11@chironja.ece.uprm.edu>
2017-02-08 12:23:10 -0400

    Changes of title, location and size of frame done

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,6 +1,7 @@
 import javax.swing.JFrame;
 
-public class Lab3Exercises {
+public class Lab3Exercises 
+{
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Maria");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
