776f744c68cc4113604c088f80a3e9682aea7051
4015c10 <4015c10@icaco.ece.uprm.edu>
2017-11-15 16:18:24 -0400

    Just commit and push.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class Lab3Exercises 
 {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Celeste");
+		JFrame myFrame = new JFrame("Maria Celeste");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900,450);
 		myFrame.setSize(200, 200);
